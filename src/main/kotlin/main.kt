9e1cdbfc44fbf63bb3b470bbead30b02908d79d5
Tarun Chauhan <artisan.tarun@gmail.com>
2021-08-27 10:10:39 +0530

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,12 @@
+import kotlin.math.*;
+
+fun main(args: Array<String>) {
+    val a:Int = 200;
+    val pi:Double = 13.14159;
+    var Salary:Int = 500000;
+    println("Hello World!");
+    println(28 % 10);
+    println(sqrt(45.00));
+    println(34* PI);
+    println(max(2,5));
+}
